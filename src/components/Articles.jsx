03850edbb8acd9e2b31b63c54b67a0bd4b0452bf
Kosta Kazakov <kostakazakoff@gmail.com>
2024-01-21 17:00:47 +0200

    Article details added

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -15,7 +15,7 @@ const Articles = () => {
         <div className="container-fluid p-5">
             <div className="accordion accordion-flush" id="accordionFlushExample">
                 {articles.map(article => (
-                    <div className="accordion-item" key={article.article_id}>
+                    <div className="accordion-item border-bottom border-dark" key={article.article_id}>
                         <h2 className="accordion-header">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${article.article_id}`} aria-expanded="false" aria-controls="flush-collapseOne">
                                 {article.description}, инвентарен № <strong>{article.inventory_number}</strong>
@@ -23,7 +23,14 @@ const Articles = () => {
                         </h2>
                         <div id={`collapse${article.article_id}`} className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
                             <div className="accordion-body">
-                                {article.inventory_number}
+                                <div>Каталожен номер: <strong>{article.catalog_number}</strong></div>
+                                <div>Чертожен номер: <strong>{article.draft_number}</strong></div>
+                                <div>Материал: <strong>{article.material}</strong></div>
+                                <div>Цена: <strong>{article.price}</strong> лв.</div>
+                                <div>Количество: <strong>{article.quantity}</strong> бр.</div>
+                                <div>Опаковка: <strong>{article.package}</strong></div>
+                                <div>Позиция: <strong>{article.position}</strong></div>
+                                <button type="button" className="btn btn-primary mt-2">Склад: <strong>{article.store_id}</strong></button>
                             </div>
                         </div>
                     </div>
