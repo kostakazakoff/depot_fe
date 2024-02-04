ebf9ce7d2499a45c47905753c96ae6a820f62259
Kosta Kazakov <kostakazakoff@gmail.com>
2024-02-04 13:23:47 +0200

    none

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
             <img src="https://www.rowse.co.uk/static/images/blog/posts/open-graph/what-is-the-digital-railway-open-graph.jpg"
                 alt="" className="img-fluid position-fixed top-0 start-0"></img>
             <div className="container-fluid d-flex justify-content-between align-items-center position-absolute top-50 start-50 translate-middle px-5 py-3 bg-warning shadow-lg">
-                <h1>БДЖ ДЕПО БУРГАС</h1>
+                <h1>СКЛАД ДЕПО</h1>
                 {!isAuthenticated &&
                     <div>
                         <Link
