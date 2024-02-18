dd069d21e2d76be2e57ebc03946c0fc24e2faddc
Kosta Kazakov <kostakazakoff@gmail.com>
2024-02-18 19:56:14 +0200

    add dropdown select menu

--- src/components/EditArticle.jsx
+++ src/components/EditArticle.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
+import { useContext } from 'react';
+import StoresContext from '../contexts/storesContext';
 
 const EditArticle = () => {
     const location = useLocation();
     const { state } = location;
     const [article, setArticle] = useState(state);
-    console.log(article.catalog_number);
+    const { stores } = useContext(StoresContext);
 
     const handleChange = (e) => {
         setArticle(state => ({
@@ -15,7 +17,7 @@ const EditArticle = () => {
     }
 
     return (
-        <form className="container-lg vertical-center position-absolute top-50 start-50 translate-middle p-5 bg-white rounded-4 shadow-lg">
+        <form className="container-sm vertical-center mt-4 p-5 bg-white rounded-4 shadow-lg">
             <div className="input-group mb-2 shadow">
                 <label className="input-group-text" id="basic-addon2" htmlFor="inventory_number">Инвентарен номер:</label>
                 <input
@@ -71,8 +73,90 @@ const EditArticle = () => {
                 />
             </div>
 
+            <div className="input-group mb-2 shadow">
+                <label className="input-group-text" id="basic-addon2" htmlFor="description">Описание:</label>
+                <input
+                    id='description'
+                    type="text"
+                    className="form-control"
+                    aria-describedby="basic-addon2"
+                    name='description'
+                    value={article.description || ''}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="input-group mb-2 shadow">
+                <label className="input-group-text" id="basic-addon2" htmlFor="price">Цена (лв.):</label>
+                <input
+                    id='price'
+                    type="text"
+                    className="form-control"
+                    aria-describedby="basic-addon2"
+                    name='price'
+                    value={article.price || ''}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="input-group mb-2 shadow">
+                <label className="input-group-text" id="basic-addon2" htmlFor="quantity">Количество (бр.):</label>
+                <input
+                    id='quantity'
+                    type="text"
+                    className="form-control"
+                    aria-describedby="basic-addon2"
+                    name='quantity'
+                    value={article.quantity || ''}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="input-group mb-2 shadow">
+                <label className="input-group-text" id="basic-addon2" htmlFor="package">Опаковка:</label>
+                <input
+                    id='package'
+                    type="text"
+                    className="form-control"
+                    aria-describedby="basic-addon2"
+                    name='package'
+                    value={article.package || ''}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="input-group mb-2 shadow">
+                <label className="input-group-text" id="basic-addon2" htmlFor="position">Позиция:</label>
+                <input
+                    id='position'
+                    type="text"
+                    className="form-control"
+                    aria-describedby="basic-addon2"
+                    name='position'
+                    value={article.position || ''}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="input-group mb-2 shadow nav-item dropdown">
+                <span className="input-group-text">Склад:</span>
+                <select
+                    id="storeSelect"
+                    className="form-select"
+                    value={article.store_id}
+                    name="store_id"
+                    onChange={handleChange}
+                >
+                    {stores.map((store) => (
+                        <option key={store.id} value={store.id}>
+                            {store.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
             <div className="mt-3">
-                <div className="container p-auto d-flex justify-content-center align-items-center shadow" style={{ height: "400px", overflow: 'hidden' }}>
+                <div className="container p-auto d-flex justify-content-center align-items-center shadow" style={{ height: "300px", overflow: 'hidden' }}>
                     <img
                         src={article.images[0] && article.images[0].url}
                         alt={article.images[0] && article.images[0].path}
