3fff29ba3afaad514cad94f2721e6ef9f196b6c4
Kosta Kazakov <kostakazakoff@gmail.com>
2024-02-23 22:39:33 +0200

    ImagePreview component - zoom created

--- src/components/ImagesPreview.jsx
+++ src/components/ImagesPreview.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLocation, Link } from "react-router-dom";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
 
 const ImagesPreview = () => {
     const location = useLocation();
@@ -7,23 +9,49 @@ const ImagesPreview = () => {
     const data = { ...state };
     const images = data.images;
 
-    const [imagePreview, setImagePreview] = useState(images[0])
+    const imagePreviewRef = useRef(images[0]);
+
+    const [img, setImg] = useState(images[0]);
+    
+    const setImagePreview = (image) => {
+        imagePreviewRef.current = image;
+        setImg(imagePreviewRef.current);
+    }
+    // const [imagePreview, setImagePreview] = useState(images[0])
 
     return (
         <section
-        className="d-flex flex-column align-items-center justify-content-center bg-light"
+            className="container-fluid d-flex flex-column align-items-center justify-content-center bg-light"
         >
             <div
-                className="container-lg p-5 mt-5 d-flex gap-4 align-items-center justify-content-center"
-                style={{ height: '80vh' }}
+                className="p-5 mt-5 d-flex gap-4 align-items-center justify-content-center"
+                style={{ width: '100%', height: '80vh' }}
             >
 
                 <div
-                    className="overflow-hidden d-flex w-75 h-75 justify-content-center rounded bg-light shadow-lg"
+                    className="overflow-hidden d-flex justify-content-center rounded align-items-center"
+                    style={{ width: '100%', height: '100%' }}
                 >
+                    <TransformWrapper
+                        initialScale={1}
+                        initialPositionX={100}
+                        initialPositionY={200}
+                        centerOnInit={true}
+                        limitToBounds={true}
+                        minScale={0.5}
+                        centerZoomedOut={true}
+                        className="object-fit-cover mw-100 mh-100"
+                    >
+                        <TransformComponent>
                     <img
-                        src={imagePreview.url}
-                        alt="" />
+                        ref={imagePreviewRef}
+                        src={imagePreviewRef.current.url}
+                        alt=""
+                        className="object-fit-contain h-100 mh-100"
+                    />
+                    </TransformComponent>
+                    </TransformWrapper>
+
                 </div>
 
                 <div
@@ -35,7 +63,7 @@ const ImagesPreview = () => {
                             style={{ width: "100px", height: "100px", overflow: 'hidden' }}
                             key={image.id}
                             type="button"
-                            onClick={() => { setImagePreview(image) }}
+                            onClick={() => setImagePreview(image)}
                         >
                             <img
                                 className="object-fit-cover w-100 h-100"
@@ -48,7 +76,7 @@ const ImagesPreview = () => {
             </div>
             <Link
                 to={data.path}
-                className="btn btn-secondary"
+                className="btn btn-secondary align-self-end me-5"
             >
                 Назад
             </Link>
