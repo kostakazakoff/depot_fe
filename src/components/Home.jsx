f9615f3a1751162cf0cb19000bffc75168934539
Kosta Kazakov <kostakazakoff@gmail.com>
2024-02-29 00:03:30 +0200

    css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,8 +10,11 @@ const Home = () => {
 
     return (
         <>
-            <img src="https://www.rowse.co.uk/static/images/blog/posts/open-graph/what-is-the-digital-railway-open-graph.jpg"
-                alt="" className="img-fluid position-fixed top-0 start-0"></img>
+            <div className="position-fixed overflow-hidden h-100 w-100">
+                <img src="https://www.rowse.co.uk/static/images/blog/posts/open-graph/what-is-the-digital-railway-open-graph.jpg"
+                    alt="" className="object-fit-cover"></img>
+            </div>
+
             <div className="container-fluid d-flex justify-content-between align-items-center position-absolute top-50 start-50 translate-middle px-5 py-3 bg-warning shadow-lg">
                 <h1>ЛОКОМОТИВНО ДЕПО БУРГАС</h1>
                 {!isAuthenticated &&
