23b91b8ffaaed21d5ee98bc41026aa254f94686f
Kosta Kazakov <kostakazakoff@gmail.com>
2024-03-08 17:04:20 +0200

    Files drop zone edited

--- src/components/EditArticle.jsx
+++ src/components/EditArticle.jsx
@@ -44,14 +44,14 @@ const EditArticle = (props) => {
 
     const removeExistedImage = id => {
         const newImages = article.images.filter(image => image.id !== id);
-        article.images = newImages;
         setArticle(state => ({
             ...state,
-            [state.images]: article.images
+            'images': newImages
         }));
         setImagesToDelete(state => ([
             ...state, id
         ]))
+        console.log(article.images);
     };
 
     const handleChange = (e) => {
@@ -309,6 +309,23 @@ const EditArticle = (props) => {
                         </div>
                     </div>
 
+                    <section
+                        {...getRootProps({
+                            className: 'w-100 border border-1 border-primary rounded p-4 position-relative',
+                        })}
+                    >
+                        <h2 className='text-primary fs-3'>Качи нови файлове</h2>
+                        <input
+                            className='text-secondary'
+                            {...getInputProps()}
+                        />
+                        {
+                            isDragActive ?
+                                <p>Пусни файла тук...</p> :
+                                <p>Провлачи и пусни файловете тук (локални файлове или от мрежата)...</p>
+                        }
+                    </section>
+
                     <section className="w-100 shadow rounded position-relative mb-3 p-4">
                         <h2 className='text-primary fs-3'>Текущи файлове</h2>
                         <div
@@ -336,51 +353,34 @@ const EditArticle = (props) => {
                                     </button>
                                 </div>
                             ))}
-                        </div>
-                    </section>
-
-                    <section
-                        {...getRootProps({
-                            className: 'w-100 shadow rounded p-4 position-relative',
-                        })}
-                    >
-                        <h2 className='text-primary fs-3'>Качи нови файлове</h2>
-                        <input
-                            className='text-secondary'
-                            {...getInputProps()}
-                        />
-                        {
-                            isDragActive ?
-                                <p>Пусни файла тук...</p> :
-                                <p>Провлачи и пусни файловете тук (локални файлове или от мрежата)...</p>
-                        }
-                    </section>
-
-                    <section className='overflow-y-auto d-flex justify-content-start align-items-center gap-4 position-relative'>
-                        {files.map(file => (
-                            <div
-                                key={file.name}
-                                className='rounded bg-light shadow position-relative overflow-hidden d-flex justify-content-center'
-                                style={{ width: '100px', height: '100px', minWidth: '100px' }}
-                            >
-                                <img
-                                    src={file.preview}
-                                    alt={file.name}
-                                    className='className="object-fit-cover h-100'
-                                    onLoad={() => {
-                                        URL.revokeObjectURL(file.preview)
-                                    }}
-                                />
-                                <button
-                                    type='button'
-                                    className='position-absolute bg-danger text-white'
-                                    style={{ right: '3px', top: '3px', borderRadius: '100%', border: 'none' }}
-                                    onClick={() => removeFile(file.name)}
-                                >
-                                    <i className="fa-solid fa-xmark"></i>
-                                </button>
+                            <div className='overflow-y-auto d-flex justify-content-start align-items-center gap-4 position-relative'>
+                                {files.map(file => (
+                                    <div
+                                        key={file.name}
+                                        className='rounded bg-light shadow position-relative overflow-hidden d-flex justify-content-center'
+                                        style={{ width: '100px', height: '100px', minWidth: '100px' }}
+                                    >
+                                        <img
+                                            src={file.preview}
+                                            alt={file.name}
+                                            className='className="object-fit-cover h-100'
+                                            onLoad={() => {
+                                                URL.revokeObjectURL(file.preview)
+                                            }}
+                                        />
+                                        <button
+                                            type='button'
+                                            className='position-absolute bg-danger text-white'
+                                            style={{ right: '3px', top: '3px', borderRadius: '100%', border: 'none' }}
+                                            onClick={() => removeFile(file.name)}
+                                        >
+                                            <i className="fa-solid fa-xmark"></i>
+                                        </button>
+                                    </div>
+                                ))}
                             </div>
-                        ))}
+                        </div>
+
                     </section>
 
                 </div>
