380829b5cbf2451f4bec93d9b5cd7f15893ad656
Kosta Kazakov <kostakazakoff@gmail.com>
2024-12-14 13:45:23 +0200

    Home view changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,12 +9,12 @@ const Home = () => {
 
     return (
         <>
-            <div className="position-fixed overflow-hidden h-100 w-100">
+            {/* <div className="position-fixed overflow-hidden h-100 w-100">
                 <img src="https://www.rowse.co.uk/static/images/blog/posts/open-graph/what-is-the-digital-railway-open-graph.jpg"
                     alt="" className="object-fit-cover"></img>
-            </div>
+            </div> */}
 
-            <div className="container-fluid d-flex justify-content-between align-items-center position-absolute top-50 start-50 translate-middle px-5 py-3 bg-warning shadow-lg">
+            <div className="container-fluid d-flex justify-content-between align-items-center position-absolute top-50 start-50 translate-middle px-5 py-3 bg-primary shadow-lg">
                 <h1>LINKER</h1>
                 {!isAuthenticated &&
                     <div>
